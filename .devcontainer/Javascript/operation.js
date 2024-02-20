69196401dc33c74a6c87f377127688b204cb5c36
Mrityunjay Kumar <pranmrityunjay@gmail.com>
2024-02-20 10:09:57 +0000

    container config done

---
+++ .devcontainer/Javascript/operation.js
@@ -0,0 +1,15 @@
+let x=3;
+let y=-x;
+let name1="mrityunjay";
+let name2=" kumar"
+console.log(y);
+console.log(name1+name2);
+let a=100;
+a++;
+console.log(a);
+console.log("mrityunjay"+1+1+2+4+6);
+console.log(1+3+10+11+"kumar"+" mrityunjay"+1029+73+1+2);
+console.log(+true);
+console.log(+"")
+
+
