e64f27bd6a24f8159bdc4e63ca34670c151129ab
Mrityunjay Kumar <pranmrityunjay@gmail.com>
2024-02-20 09:51:17 +0000

    container config done

---
+++ .devcontainer/Javascript/conversionOperation.js
@@ -0,0 +1,19 @@
+let x=10;
+let name=String(x);
+console.log(typeof(name));
+let y="1000abs";
+let m=Number(y);
+console.log(typeof(m));
+console.log(m);
+let flag=true;
+let convert=Number(flag);
+console.log(typeof(convert));
+console.log(convert);
+
+let name1="";
+let check=Boolean(name1);
+console.log(check);
+
+
+
+
