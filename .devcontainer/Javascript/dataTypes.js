4afe38ac60fe1747c595138d8c2672c591d69746
Mrityunjay Kumar <pranmrityunjay@gmail.com>
2024-02-20 09:29:14 +0000

    container config done

---
+++ .devcontainer/Javascript/dataTypes.js
@@ -0,0 +1,9 @@
+let x = BigInt("123456789012345678901234567890");
+let flag=Boolean(true);
+ let marks=Number(99);
+ let value=null;
+ let y=undefined;
+ let name1=String("mrityunjay");
+
+console.table([x,flag,marks,value,y,name1]);
+
---
+++ .devcontainer/Javascript/variable.js
@@ -0,0 +1,13 @@
+const id=111155;
+let name="mrityunjay kumar";
+var marks=101;
+Email_id="pranmrityunjay@gmail.com";
+console.log(id);
+console.table([id,marks,name]);
+/* please do not use var*/
+
+// id=111;
+
+name="jay";
+marks=201;
+console.table([name,marks]);
