34ae8f85a90c9fea1c2abba744af151a3ea133b0
Mrityunjay Kumar <pranmrityunjay@gmail.com>
2024-02-21 16:47:12 +0000

    container config done

--- .devcontainer/Javascript/function.js
+++ .devcontainer/Javascript/function.js
@@ -60,8 +60,31 @@ passingAnObject(myObj)
 // passingAnObject({name:"mrityunjay",email:"jay@gmail.com"})
 
 
-function passingAnArray(arr){
-    console.log(arr[0],arr[2])
+// function passingAnArray(arr){
+//     console.log(arr[0],arr[2])
+// }
+
+// passingAnArray([0,2,3,4,4])
+
+let a=10;
+
+if(a==a){
+a=100
 }
 
-passingAnArray([0,2,3,4,4])
+console.log(a)
+
+// for(int i=0;i<2;i++)
+
+console.log(find1(9))
+
+// function find1(num){
+//     return num+1;
+
+// }
+
+console.log(find2(5))
+let ans=function(num){
+    return num+1;
+}
+
---
+++ .devcontainer/Javascript/thisAndArrowFunction.js
@@ -0,0 +1,50 @@
+// let myObj={
+//     name:"mrityunjay",
+//     email:"pranmrityunjay@gmail.com",
+
+//     UserName:function(){
+//         // console.log(`${this.name}, this is userName`)
+//         console.log(this)
+//     }
+
+
+
+// }
+
+//  myObj.UserName()
+//  myObj.name="jay"
+//  myObj.UserName()
+
+
+//  console.log(this)
+
+//  function This(){
+//     username:"mrityunjay";
+//     console.log(this);
+//  }
+
+//  This()
+
+
+
+//Arrow function
+
+// let ArrowFunction=function(){
+//     username:"mrityunay"
+//     console.log(this)
+// }
+
+// ArrowFunction()
+
+
+// let ArrowFunction=()=>{
+//     username:"mrityunay"
+//     console.log(this)
+// }
+
+// ArrowFunction()
+
+
+let ArrowFunction=(num1,num2)=> ({key:"value"})
+
+console.log(ArrowFunction(12,98))
