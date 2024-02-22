c8f2ad82071c733c196013428f39b721c87a70e6
Mrityunjay Kumar <pranmrityunjay@gmail.com>
2024-02-22 12:51:31 +0000

    container config done

---
+++ .devcontainer/Javascript/truthy.js
@@ -0,0 +1,10 @@
+// true,[],{}," ",0==false, 1==true,function (){},
+// false,"",0,BigInt,null,undefined,"0","false"
+
+//Nullish coalescing operator (??)
+
+let x=null??10
+console.log(x)
+
+let y=undefined??12
+console.log(y)
