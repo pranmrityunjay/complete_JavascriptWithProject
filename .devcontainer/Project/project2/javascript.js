245c5dd590d3246814aa9a7cd3214985e8a64649
Mrityunjay Kumar <pranmrityunjay@gmail.com>
2024-02-24 20:37:59 +0530

    container config done

--- .devcontainer/Project/project2/javascript.js
+++ .devcontainer/Project/project2/javascript.js
@@ -8,20 +8,27 @@ form.addEventListener("submit",(E)=>{
   result.innerHTML="Height is Invalid"
   else if(weight<=0 || isNaN(weight) ||weight==="")
   result.innerHTML="weight is Invalid"
-
+  else{
   let h=height*height
   const bmi=(10000*weight/h).toFixed(2)
-  if(bmi<18.6)
-  result.innerHTML="underWeight"
-  else if(bmi<24.9)
-  result.innerHTML="normal range"
-  else
-  result.innerHTML="overWeight"
+  if(bmi<18.6){
+  result.innerHTML="Under-weight"
+  result.style.color="red"
+  }
+  else if(bmi<24.9){
+  result.innerHTML="Normal Range"
+  result.style.color="Green"
+  }
+  else{
+  result.innerHTML="Over-weight"
+  result.style.color="red"
+  }
+}
 
 
 
 
-  
+
 
 
 
