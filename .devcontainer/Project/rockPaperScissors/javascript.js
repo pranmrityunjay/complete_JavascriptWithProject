1ea97123226f2f73d7757b41f32e1b86df5b0dd9
Mrityunjay Kumar <pranmrityunjay@gmail.com>
2024-02-25 23:47:43 +0530

    container config done

--- .devcontainer/Project/rockPaperScissors/javascript.js
+++ .devcontainer/Project/rockPaperScissors/javascript.js
@@ -1,87 +1,56 @@
-let userScore=0;
-let compScore=0;
+let userScore = 0;
+let compScore = 0;
 
-prevScore=document.querySelector("#user-score");
-L=document.querySelector("#Computer-score")
+prevScore = document.querySelector("#user-score");
+L = document.querySelector("#Computer-score");
 
+const small = document.querySelectorAll('.small');
+const message = document.querySelector("#message");
 
-const small=document.querySelectorAll('.small')
-const message=document.querySelector("#message")
-
-const ShowWinner=(userWin)=>{
-    if(userWin){
+const ShowWinner = (userWin) => {
+    if (userWin) {
         userScore++;
-        message.innerHTML="You Win"
-        prevScore.innerHTML=userScore;
-
-    
-    }
-    else{
+        message.innerHTML = "You Win";
+        prevScore.innerHTML = userScore;
+    } else {
         compScore++;
-        L.innerHTML=compScore;
-
-    message.innerHTML="compuerWin"
+        L.innerHTML = compScore;
+        message.innerHTML = "Computer Win";
     }
-    
+};
 
-}
-const drawGame=()=>{
-    message.innerHTML="game draw"
+const drawGame = () => {
+    message.innerHTML = "Game draw";
+};
 
-}
-const genComputerChoice=()=>{
-    const option=["rock","paper","scissors"]
-    const temp=Math.floor((Math.random())*3);
-    return option[temp];
-}
-const playGame=(userChoice)=>{
-    const compChoice=genComputerChoice();
-    if(userChoice==compChoice)
-    drawGame();
-else{
-    let userWin=true;
-    if(userChoice=="rock"){
-        if(compChoice=="paper")
-        userWin = compChoice === "paper" ? false : true;
+const genComputerChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const temp = Math.floor(Math.random() * 3);
+    return options[temp];
+};
 
+const playGame = (userChoice) => {
+    const compChoice = genComputerChoice();
+    if (userChoice == compChoice) drawGame();
+    else {
+        let userWin = true;
+        if (userChoice == "rock") {
+            if (compChoice == "paper") userWin = false;
+        } else if (userChoice == "paper") {
+            if (compChoice == "rock") userWin = true;
+            if (compChoice == "scissors") userWin = false;
+        } else {
+            if (compChoice == "paper") userWin = true;
+            if (compChoice == "rock") userWin = false;
+        }
+        ShowWinner(userWin);
     }
-    else if(userChoice=="paper"){
-        if(compChoice=="rock")
-        userWin=true;
-        if(compChoice=="scissors")
-        userWin=false;
+};
 
-
-    }
-
-    else{
-        if(compChoice=="paper")
-        userWin=true;
-        if(compChoice=="rock")
-        userWin=false;
-
-
-    }
-
-    ShowWinner(userWin);
-    
-}
-    
-
-
-}
-small.forEach((each)=>{
-    // console.log(each)
-    each.addEventListener("click",(E)=>{
-        // const id=each.getAttribute('id')
-        const userChoice=each.getAttribute("id")
-        playGame(userChoice)
-
-
-
-        
-        
-
-    })
-
-})
+small.forEach((each) => {
+    each.addEventListener("click", (E) => {
+        const userChoice = each.getAttribute("id");
+        playGame(userChoice);
+        console.log("Clicked on", userChoice);
+    });
+});
